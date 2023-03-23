56dab5d3d290e82eed90a5d5cabeabd2a0468fcd
Annashx <ana.clara10@aluno.ifce.edu.br>
2023-03-23 16:55:46 -0300

    23 de março de 2023

--- airline/flights/urls.py
+++ airline/flights/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path("<int:flight_id>", views.flight, name="flight")
 ]
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -6,4 +6,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('')
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(id=flight_id)
+    return render(request, "flights/flights.html", {
+        "flight": flight
+    })
